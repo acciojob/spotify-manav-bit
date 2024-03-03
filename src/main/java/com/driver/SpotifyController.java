73a5a532931610c527c670fe667c8ee35799692b
MANAV AHUJA <ahujamanav43@gmail.com>
2024-03-04 01:37:59 +0530

    ans3

--- src/main/java/com/driver/SpotifyController.java
+++ src/main/java/com/driver/SpotifyController.java
@@ -34,7 +34,7 @@ spotifyService.createAlbum(title,artistName);
     }
 
     @PostMapping("/add-song")
-    public String createSong(String title, String albumName, int length) throws Exception{
+    public String createSong(@RequestParam String title, String albumName, int length) throws Exception{
         //If the album does not exist in database, throw "Album does not exist" exception
         //Create and add the song to respective album
 spotifyService.createSong(title,albumName,length);
@@ -42,7 +42,7 @@ spotifyService.createSong(title,albumName,length);
     }
 
     @PostMapping("/add-playlist-on-length")
-    public String createPlaylistOnLength(String mobile, String title, int length) throws Exception{
+    public String createPlaylistOnLength(@RequestParam String mobile, String title, int length) throws Exception{
         //Create a playlist with given title and add all songs having the given length in the database to that playlist
         //The creater of the playlist will be the given user and will also be the only listener at the time of playlist creation
         //If the user does not exist, throw "User does not exist" exception
@@ -51,7 +51,7 @@ spotifyService.createPlaylistOnLength(mobile,title,length);
     }
 
     @PostMapping("/add-playlist-on-name")
-    public String createPlaylistOnName(String mobile, String title, List<String> songTitles) throws Exception{
+    public String createPlaylistOnName(@RequestParam String mobile, String title, List<String> songTitles) throws Exception{
         //Create a playlist with given title and add all songs having the given titles in the database to that playlist
         //The creater of the playlist will be the given user and will also be the only listener at the time of playlist creation
         //If the user does not exist, throw "User does not exist" exception
@@ -60,26 +60,26 @@ spotifyService.createPlaylistOnName(mobile,title,songTitles);
     }
 
     @PutMapping("/find-playlist")
-    public String findPlaylist(String mobile, String playlistTitle) throws Exception{
+    public Playlist findPlaylist(@RequestParam String mobile, String playlistTitle) throws Exception{
         //Find the playlist with given title and add user as listener of that playlist and update user accordingly
         //If the user is creater or already a listener, do nothing
         //If the user does not exist, throw "User does not exist" exception
         //If the playlist does not exists, throw "Playlist does not exist" exception
         // Return the playlist after updating
-spotifyService.findPlaylist(mobile, playlistTitle);
-        return "Success";
+return spotifyService.findPlaylist(mobile, playlistTitle);
+
     }
 
     @PutMapping("/like-song")
-    public String likeSong(String mobile, String songTitle) throws Exception{
+    public Song likeSong(@RequestParam String mobile, String songTitle) throws Exception{
         //The user likes the given song. The corresponding artist of the song gets auto-liked
         //A song can be liked by a user only once. If a user tried to like a song multiple times, do nothing
         //However, an artist can indirectly have multiple likes from a user, if the user has liked multiple songs of that artist.
         //If the user does not exist, throw "User does not exist" exception
         //If the song does not exist, throw "Song does not exist" exception
         //Return the song after updating
-spotifyService.likeSong(mobile,songTitle);
-        return "Success";
+return spotifyService.likeSong(mobile,songTitle);
+
     }
 
     @GetMapping("/popular-artist")
@@ -91,6 +91,6 @@ return spotifyService.mostPopularArtist();
     @GetMapping("/popular-song")
     public String mostPopularSong(){
         //return the song title with maximum likes
-return spotifyService.mostPopularSong();
+ return spotifyService.mostPopularSong();
     }
 }
